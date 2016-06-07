01e0a1f426a58be8d06961a2e2e374ed5872a752
Johan Frølich <johanfrolich@gmail.com>
2016-06-07 15:58:29 +0200

    Copied from omniauth-bonnier-oauth2

---
+++ README.md
@@ -0,0 +1,27 @@
+#OmniAuth Bonnier OAuth2
+
+Strategy to authenticate Bonnier Admin in OmniAuth.
+
+It's using the OAuth2 from the Bonnier Admin API
+
+##Usage
+Add the strategy to your `Gemfile` alongside OmniAuth:
+
+```ruby
+gem 'omniauth'
+gem 'omniauth-bonnier-admin-oauth2'
+```
+
+Integrate this strategy to your OmniAuth middleware.
+
+```ruby
+use OmniAuth::Builder do
+  provider :bonnier_admin_oauth2, ENV['BONNIER_KEY'], ENV['BONNIER_SECRET']
+end
+```
+
+You need to add your key and secret.
+
+Also remember to add the full callback path to your Bonnier App Console, for example: http://example.com/auth/bonnier-oauth2/callback
+
+For more information check the [OmniAuth wiki](https://github.com/intridea/omniauth/wiki).
---
+++ lib/omniauth/strategies/bonnier_admin_oauth2.rb
@@ -0,0 +1,42 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class BonnierAdminOauth2 < OmniAuth::Strategies::OAuth2
+      args [:client_id, :client_secret, :site]
+
+      option :site
+      option :name, "bonnier_admin_oauth2"
+      option :client_options, {
+        :authorize_url      => '/api/oauth/authorize',
+        :token_url          => '/api/oauth/token'
+      }
+
+      uid { raw_info["id"].to_s }
+
+      info do
+        {
+          "email" => raw_info["email"],
+          "username" => raw_info["username"],
+          "profile_image_url" => raw_info["profile_image"].try(:fetch, "url"),
+          "url" => raw_info["url"],
+          "roles" => raw_info["roles"]
+        }
+      end
+
+      extra do
+        {
+          'raw_info' => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/api/v2/users/current.json').parsed
+      end
+
+      def client
+        ::OAuth2::Client.new(options.client_id, options.client_secret, deep_symbolize(options.client_options).merge(site: options.site))
+      end
+    end
+  end
+end
